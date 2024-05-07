a9f3af98b5d1533c579c70bc5ea1597605cb82db
kimdy108 <kimbrothers123@daum.net>
2024-05-07 22:52:38 +0900

    2024.05.07 게시글 목록 및 상세 페이지

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -5,13 +5,26 @@ import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
     @Autowired
     private BoardRepository boardRepository;
 
+    // 게시글 작성
     public void write(Board board) {
         boardRepository.save(board);
+    }
+
+    // 게시글 목록
+    public List<Board> boardList () {
+        return boardRepository.findAll();
+    }
+
+    // 게시글 상세
+    public Board boardView (Integer id) {
+        return boardRepository.findById(id).get();
     }
 }
