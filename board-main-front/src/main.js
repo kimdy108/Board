52dbe33add8fe135ffec550d4702ad30cd5d386b
kimdy <kimdy108@vizufon.co.kr>
2024-09-22 16:08:04 +0900

    2024.09.22 pinia, primevue, dayjs, router add

--- board-main-front/src/main.js
+++ board-main-front/src/main.js
@@ -1,7 +1,20 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import dayjs from './plugins/dayjs'
+import router from '@/router'
 import App from './App.vue'
 
 const app = createApp(App)
 app.mount('#app')
+app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+app.use(createPinia())
+app.use(dayjs)
