dd0388b9f326ad17482546a60ac4086bae40f625
kimdy <kimdy108@vizufon.co.kr>
2024-09-22 15:41:24 +0900

    2024.09.22 first initial

---
+++ board-main-api/src/main/java/com/project/board/main/api/BoardMainApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.board.main.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardMainApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardMainApiApplication.class, args);
+    }
+
+}
---
+++ board-main-front/.eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: '2022',
+    sourceType: 'module'
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': off
+  }
+}
