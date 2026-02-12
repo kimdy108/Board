553aa20751c6193e652da2fbd14fd9eae066da7b
kimdy108 <kimbrothers123@daum.net>
2026-02-12 18:40:10 +0900

    2026.02.12 1.0.0 Delete

--- board-main-front-1.0.0/src/reference/config.js
+++
@@ -1,5 +0,0 @@
-const apiMethod = 'http'
-const apiUrl = window.location.hostname
-const apiPort = import.meta.env.VITE_APP_API_PORT
-
-export const baseUrl = `${apiMethod}://${apiUrl}:${apiPort}/api`
--- board-main-ui-1.0.0/src/main/resources/static/assets/board-BFWOEN-2.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/board-OYhW72JQ.png";export{s as _};
